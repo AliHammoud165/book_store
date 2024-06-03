6ecd746662a5e31ed63fbaf4232ac8cf3f992f9c
AliHammoud165 <158801929+AliHammoud165@users.noreply.github.com>
2024-06-03 19:46:13 +0300

    .

--- library/src/main/java/com/library/library/Author/AuthorRepositpry.java
+++
@@ -1,7 +0,0 @@
-package com.library.library.Author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepositpry extends JpaRepository<Author,Integer> {
-
-}
